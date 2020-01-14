fca2be73061d52f1e40d04be47fdb6eafdc202f0
kyisoelynn <kyisoelynn@gmail.com>
2020-01-14 18:15:34 +0630

    minor change

--- search.php
+++ search.php
@@ -3,15 +3,24 @@ if (isset($_GET['q'])) {
     $search = validate_data($_GET['q']);
     $handle = fopen('dummy_text.txt', 'r');
     $line_number = 1;
-    $result = [];
+    $found = false;
+
     if ($handle) {
         while (($line = fgets($handle)) !== FALSE) {
 
             $result = search($search, $line, 101);
             if ($result) {
-                echo('Searched word is found at ' . $result . ' in line number: ' . $line_number . '<br>');
+                $found = true;
+                foreach ($result as $index) {
+                    echo('Searched word is found at ' . $index . ' in line number: ' . $line_number . '.<br>');
+                }
             }
             $line_number++;
+        }
+
+        if (!$found) {
+            echo("Sorry. Your search word can't be found in the file.<br>");
+            echo("<a href='index.php'>Go Back</a>");
         }
         
         fclose($handle);
@@ -33,19 +42,21 @@ function search($search, $text, $q){
     $search_hash = 0;
     $text_hash = 0;
 
-    $d = 1;
+    $d = 256;
     $h = 1;
 
     for ($i=0; $i < $search_length-1; $i++) { 
         $h = ($h * $d) % $q;
     }
 
-    for ($i=0; $i < $search_length; $i++) { 
+    for ($i=0; $i < $search_length; $i++) {
         $search_hash = ($d * $search_hash + ord($search[$i])) % $q;
         $text_hash = ($d * $text_hash + ord($text[$i])) % $q;
     }
 
-    for ($i=0; $i < $text_length - $search_length; $i++) { 
+    $index = [];
+
+    for ($i=0; $i <= $text_length - $search_length; $i++) { 
         
         if ($search_hash == $text_hash) {
             for ($j=0; $j < $search_length; $j++) { 
@@ -54,14 +65,17 @@ function search($search, $text, $q){
                 }
             }
             if ($j == $search_length) {
-                return $i;
+                $index[] = $i;
             }
         }
         if ($i < $text_length - $search_length) {
+
+            // $text_hash = ($d * ($t - $txt[$i] * $h) + $txt[$i + $M]) % $q;
             $text_hash = ($d * ($text_hash - ord($text[$i]) * $h) + ord($text[$i + $search_length])) % $q;
             if ($text_hash < 0) {
-                $text_hash += $q;
+                $text_hash = ($text_hash + $q);
             }
         }
     }
+    return $index;
 }
