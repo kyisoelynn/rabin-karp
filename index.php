1b8de57a6201d82d51cbfb2bbe42ccb3fa7ab805
kyisoelynn <kyisoelynn@gmail.com>
2020-01-14 17:07:43 +0630

    first stage

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Search Test</title>
+</head>
+<body>
+    <form action="search.php" method="get">
+        <div>
+            <input type="text" name="q" required>
+            <input type="submit" value="Search">
+        </div>
+    </form>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,67 @@
+<?php 
+if (isset($_GET['q'])) {
+    $search = validate_data($_GET['q']);
+    $handle = fopen('dummy_text.txt', 'r');
+    $line_number = 1;
+    $result = [];
+    if ($handle) {
+        while (($line = fgets($handle)) !== FALSE) {
+
+            $result = search($search, $line, 101);
+            if ($result) {
+                echo('Searched word is found at ' . $result . ' in line number: ' . $line_number . '<br>');
+            }
+            $line_number++;
+        }
+        
+        fclose($handle);
+    }
+}
+
+function validate_data($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+//Rabin-Karp Pattern Search Algorithm
+function search($search, $text, $q){
+    $search_length = strlen($search);
+    $text_length = strlen($text);
+
+    $search_hash = 0;
+    $text_hash = 0;
+
+    $d = 1;
+    $h = 1;
+
+    for ($i=0; $i < $search_length-1; $i++) { 
+        $h = ($h * $d) % $q;
+    }
+
+    for ($i=0; $i < $search_length; $i++) { 
+        $search_hash = ($d * $search_hash + ord($search[$i])) % $q;
+        $text_hash = ($d * $text_hash + ord($text[$i])) % $q;
+    }
+
+    for ($i=0; $i < $text_length - $search_length; $i++) { 
+        
+        if ($search_hash == $text_hash) {
+            for ($j=0; $j < $search_length; $j++) { 
+                if ($text[$i + $j] !== $search[$j]) {
+                    break;
+                }
+            }
+            if ($j == $search_length) {
+                return $i;
+            }
+        }
+        if ($i < $text_length - $search_length) {
+            $text_hash = ($d * ($text_hash - ord($text[$i]) * $h) + ord($text[$i + $search_length])) % $q;
+            if ($text_hash < 0) {
+                $text_hash += $q;
+            }
+        }
+    }
+}
